5d4cc21a54d8aad04123146defef5248289e5a16
ardiandp <ardiandp@gmail.com>
2024-08-14 14:09:51 +0700

    theme oke

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return view('layouts.app');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
